d80d8bd3e8d5741633c86c7e95786d85c7f15e8f
Challa Sneha <challasneha005@gmail.com>
2025-07-19 14:48:30 +0530

    LargestElement.cpp

---
+++ Arrays/LargestElement.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+//LARGEST ELEMENT
+//brute sort and large  (O(n log n))
+//better (o(n))
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin >> arr[i];
+    int large = arr[0];
+    for(int i=0;i<n;i++){
+        if (arr[i]>large)
+            large = arr[i];
+    }
+    cout << large ;
+    return 0;
+}
