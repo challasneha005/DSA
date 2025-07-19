cba47ce04e28ecf6e6cc2c51b01b83cbc4780d66
Challa Sneha <challasneha005@gmail.com>
2025-07-19 14:56:13 +0530

    Create LinearSearch.cpp

---
+++ Arrays/LinearSearch.cpp
@@ -0,0 +1,30 @@
+//LINEAR SEARCH
+//brute O(n) 
+void linearSearchBrute(int arr[],int n,int target){
+    for(int i=0;i<n;i++){
+        if(arr[i]==target)
+            cout << target << " index "<<i;
+    }
+}
+//Optimal O(n)
+void linearSearchOptimal(int arr[],int n,int target){
+    for(int i=0;i<n;i++){
+        if(arr[i]==target){
+            cout << target << " index "<<i;
+            return ;
+        }
+    }
+    cout << "Not found" ;
+}
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin >> arr[i];
+    int target;
+    cin >> target;
+    //linearSearchBrute(arr,n,target);
+    linearSearchOptimal(arr,n,target);
+    return 0;
+}
