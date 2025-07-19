277054e64282cb4c2dfd4f57054d3ded99a9a0e8
Challa Sneha <challasneha005@gmail.com>
2025-07-19 15:29:30 +0530

    Create Basics.cpp

---
+++ BasicsInCPP/Basics.cpp
@@ -0,0 +1,41 @@
+// Basic CPP
+// Save file as file.cpp
+// How to run: g++ file.cpp -o file
+// next: ./file
+// #include<iostream>
+// int main(){
+//     std:: cout << "Hello";
+//     std:: cout << "hi" << "\n";
+//     std:: cout << "line";
+//     return 0;
+// }
+
+
+//no need to write std every time
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout << "Hello";
+//     cout << "hi" << "\n";
+//     cout << "line" << endl; 
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a;
+//     int b;
+//     cin >> a >> b;
+//     cout << a+b << endl;
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;
+  getline(cin,s);
+  cout << s << endl;
+  return 0;
+}
