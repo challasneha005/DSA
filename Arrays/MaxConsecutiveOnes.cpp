72afe44123967af20acd7b2b6c37eef8d79a9f83
Challa Sneha <challasneha005@gmail.com>
2025-07-19 15:00:58 +0530

    Create MaxConsecutiveOnes.cpp

---
+++ Arrays/MaxConsecutiveOnes.cpp
@@ -0,0 +1,21 @@
+//MAX CONSECUTIVE ONES
+int maxConsecutiveOnes(int a[],int n){
+    int maxOnes = 0,cnt=0;
+    for(int i=0;i<n;i++){
+        if(a[i]==1){
+            cnt++;
+            maxOnes = max(maxOnes,cnt);
+        }
+        else cnt =0;
+    }
+    return maxOnes;
+}
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin >> a[i];
+    int maxOnes=maxConsecutiveOnes(a,n);
+    cout << maxOnes;;
+}
