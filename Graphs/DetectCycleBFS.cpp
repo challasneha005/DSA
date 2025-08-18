7cb158ca0df6badbc2888fee320da3c360ab0e9f
Challa Sneha <challasneha005@gmail.com>
2025-08-18 14:46:50 +0530

    Update DetectCycleBFS.cpp

--- Graphs/DetectCycleBFS.cpp
+++ Graphs/DetectCycleBFS.cpp
@@ -1,4 +1,4 @@
- //DETECT CYCLE IN UNDIRECTED GRAPGH BFS O(n+2E) ,  O(3n+2E)
+  //DETECT CYCLE IN UNDIRECTED GRAPGH BFS O(n+2E) ,  O(n+2E)
 #include<bits/stdc++.h>
 using namespace std;
 bool detect(int sr,vector<vector<int>> &a,int vis[]){
@@ -23,25 +23,23 @@ bool detect(int sr,vector<vector<int>> &a,int vis[]){
 int main(){
     int n,e;
     cin >> n >> e;
-    vector<vector<int>> a(n);
+    vector<vector<int>> a(n+1);
     for(int i=0;i<e;i++){
         int u,v;
         cin >> u >> v;
-        u--;
-        v--;
         a[u].push_back(v);
         a[v].push_back(u);
     }
-    for (int i = 0; i < n; i++) {
-        cout << i+1 << ": ";
+    for (int i = 1; i <= n; i++) {
+        cout << i << ": ";
         for (int neighbor : a[i]) {
             cout << neighbor << " ";
         }
         cout << endl;
     }
-    int vis[n] = {0};
+    int vis[n+1] = {0};
     bool hasCycle = false;
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         if(!vis[i]) {
             if(detect(i, a, vis)) {
                 hasCycle = true;
@@ -55,3 +53,11 @@ int main(){
         cout << "No cycle in the graph." << endl;
     return 0;
 }
+// 7 7
+// 1 2
+// 1 3
+// 2 5 
+// 3 6
+// 3 4
+// 5 7
+// 6 7
