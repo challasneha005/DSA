38c98d1b8f9eca6ac0b3eec5ad069fe8578c4856
Challa Sneha <challasneha005@gmail.com>
2025-11-27 18:01:46 +0530

    Add files via upload

---
+++ Dynamic Programming/LongestStringChain.cpp
@@ -0,0 +1,143 @@
+// LONGEST  STRING CHAIN
+#include<bits/stdc++.h>
+using namespace std;
+bool checkPossible(string &s1,string &s2){
+    if(s1.size()!= s2.size()+1)
+        return false;
+    int first = 0;
+    int second = 0;
+    while(first<s1.size()){
+        if(second < s2.size() && s1[first] == s2[second]){
+            first++;
+            second++;
+        }
+        else{
+            first++;
+        }
+    }
+    if(first == s1.size() && second == s2.size())
+        return true;
+    return false;
+}
+bool comp(string &s1,string &s2){
+    return s1.size()<s2.size();
+}
+// recursion O(2^n) , O(n)
+int lsc(int ind,int prevInd,vector<string> &a,int n){
+    if(ind == n)
+        return 0;
+    int nottake = lsc(ind+1,prevInd,a,n);
+    int take = 0;
+    if(prevInd == -1 ||  checkPossible(a[ind],a[prevInd]))
+        take = 1+lsc(ind+1,ind,a,n);
+    return max(nottake,take); 
+}
+//  dp O(n*n) , O(n*n)+O(n)
+int dplsc(int ind,int prevInd,vector<string> &a,int n,vector<vector<int>> &dp){
+    if(ind == n) //shift
+        return 0;
+    if(dp[ind][prevInd+1]!=-1)
+        return dp[ind][prevInd+1];
+    int nottake = dplsc(ind+1,prevInd,a,n,dp);
+    int take = 0;
+    if(prevInd == -1 || checkPossible(a[ind],a[prevInd]))
+        take = 1+dplsc(ind+1,ind,a,n,dp);
+    return dp[ind][prevInd+1] = max(nottake,take); 
+}
+// tab
+int tab(vector<string> &a,int n){
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    for(int ind=n-1;ind>=0;ind--){
+        for(int prevInd = ind-1;prevInd>=-1;prevInd--){
+            int nottake = dp[ind+1][prevInd+1];
+            int take = 0;
+            if(prevInd == -1 || checkPossible(a[ind],a[prevInd]))
+                take = 1+dp[ind+1][ind+1];
+            dp[ind][prevInd+1] = max(nottake,take); 
+        }
+    }
+    return dp[0][0];
+}
+// space O(n*n) , O(2*n)
+int spacetab(vector<string> &a,int n){
+    vector<int> next(n+1,0),cur(n+1,0);
+    for(int ind=n-1;ind>=0;ind--){
+        for(int prevInd = ind-1;prevInd>=-1;prevInd--){
+            int nottake = next[prevInd+1];
+            int take = 0;
+            if(prevInd == -1 || checkPossible(a[ind],a[prevInd]))
+                take = 1+next[ind+1];
+            cur[prevInd+1] = max(nottake,take); 
+        }
+        next = cur;
+    }
+    return next[0];
+}
+// ALGORITHMIC APPROACH   
+// one dp array O(n*n),O(n)
+int lscdp1(vector<string> &a,int n){
+    vector<int> dp(n,1);
+    int maxi = 1;
+    for(int i=0;i<n;i++){
+        for(int prev=0;prev <=i-1;prev++){
+            if(checkPossible(a[i],a[prev]))
+                dp[i] = max(dp[i],1+dp[prev]);
+        }
+        maxi = max(maxi,dp[i]);
+    }
+    return maxi;
+}
+// PRINT lIST OF lsc 
+int lscprint(vector<string> &a,int n){
+    vector<int> dp(n,1),hash(n);
+    int maxi = 1;
+    int lastInd = 0;
+    for(int i=0;i<n;i++){
+        hash[i] = i;
+        for(int prev=0;prev <=i-1;prev++){
+            if( checkPossible(a[i],a[prev]) && dp[i] < 1+dp[prev] ){
+                dp[i] = 1+dp[prev];
+                hash[i] = prev;
+            }
+        }
+        if(dp[i] > maxi){
+            maxi = dp[i];
+            lastInd = i;
+        }
+    }
+    vector<string> temp;
+    temp.push_back(a[lastInd]);
+    while(hash[lastInd] != lastInd){
+        lastInd = hash[lastInd];
+        temp.push_back(a[lastInd]);
+    }
+    reverse(temp.begin(),temp.end());
+    for(auto it:temp)
+        cout << it << " ";
+    cout << endl;
+    return maxi;
+}
+// USING BINARY SEARCH  wont work
+int main(){
+    int n;
+    cin >>n;
+    vector<string> a(n);
+    for(int i=0;i<n;i++)
+        cin >> a[i];
+
+    sort(a.begin(),a.end(),comp);
+    cout << lsc(0,-1,a,n) << endl;
+
+    vector<vector<int>> dp(n,vector<int>(n+1,-1));
+    cout << dplsc(0,-1,a,n,dp) << endl;
+
+    cout << tab(a,n) << endl;
+
+    cout << spacetab(a,n) << endl;
+
+    cout << lscdp1(a,n) << endl;
+
+    cout << lscprint(a,n) << endl;
+
+    return 0;
+}
