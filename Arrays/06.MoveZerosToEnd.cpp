02f63c3e43b81f4bdd5402d5744b41312be8bc73
Challa Sneha <challasneha005@gmail.com>
2026-02-06 10:41:33 +0530

    Add files via upload

---
+++ Arrays/06.MoveZerosToEnd.cpp
@@ -0,0 +1,58 @@
+//MOVING ZEROES TO END
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//brute  O(2n)
+void moveZeroesBrute(int arr[],int n){
+    vector <int> temp;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0)
+            temp.push_back(arr[i]);
+    }
+    for(int i=0;i<temp.size();i++)
+        arr[i]=temp[i];
+    int nonzeroIndex = temp.size();
+    for(int i=nonzeroIndex;i<n;i++)
+        arr[i]=0;
+     
+}
+//optimal 
+
+// TIME COMPLEXITY = O(N) (as we moving j throught the array only once)
+// SPACE COMPLEXITY = O(0)
+
+/*
+APPROACH:-
+-> The idea is while traversing the array if we found any zero then we have to swap it with next non-zero
+*/
+
+void moveZeroesOptimal(int arr[],int n){
+    int j=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i] == 0){
+            j=i;
+            break;
+        }
+    }
+    if(j==-1) cout<<"no zeroes";
+    for(int i=j+1;i<n;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin >> arr[i];
+    //  moveZeroesBrute(arr, n);
+    moveZeroesOptimal(arr,n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    return 0;
+}
